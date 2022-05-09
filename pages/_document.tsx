ec9546c49cb4c27629199d7bb6bb92fbd0a99624
Jack Merrill <8814123+jackmerrill@users.noreply.github.com>
2022-05-08 21:38:01 -0500

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,40 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <body>
+          <Main />
+          <NextScript />
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              <noscript>
+                <img src="https://shy.maatt.ch/ingress/6ca8c840-0db8-42ce-91b1-2b185abca277/pixel.gif" />
+              </noscript>
+              <script
+                defer
+                src="https://shy.maatt.ch/ingress/6ca8c840-0db8-42ce-91b1-2b185abca277/script.js"
+                type="text/javascript"
+              ></script>
+            </>
+          )}
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
