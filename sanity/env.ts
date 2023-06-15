9a8d072064a0565bc7b64e22a54ae0e36c19d81f
Jack Merrill <me@jackmerrill.com>
2023-06-14 23:05:56 -0500

    add favicon

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,12 +2,13 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-05-25";
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET,
   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+    process.env.SANITY_STUDIO_PROJECT_ID,
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
