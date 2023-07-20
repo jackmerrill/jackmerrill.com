d142db7fe46b0c22d1ee8d8adee818ecbfeaeaa0
Jack Merrill <me@jackmerrill.com>
2023-07-19 21:09:21 -0500

    feat: add matrix redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/matrix/:slug*",
+        destination: "https://matrix.jackmerrill.com/.well-known/matrix/:slug*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
