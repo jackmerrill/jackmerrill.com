0417af9e96c936c31ae7069107e658ac4a7dbf50
Jack Merrill <me@jackmerrill.com>
2023-11-01 12:20:22 -0400

    feat: add image optimization

--- src/components/PictureShowcase.tsx
+++ src/components/PictureShowcase.tsx
@@ -1,26 +1,7 @@
 import { useState } from "react";
 import Carousel, { type Picture } from "./Carousel";
 
-const pictures: Picture[] = [
-  {
-    title: "Drone Picture of Hampshire College",
-    image: "/img/DJI_0565.JPG",
-  },
-  {
-    title: "Drone Picture of Hampshire College (winter)",
-    image: "/img/DJI_0605.JPG",
-  },
-  {
-    title: "Bean!!",
-    image: "/img/IMG_0196.JPG",
-  },
-  {
-    title: "Ricky!!",
-    image: "/img/IMG_3942.JPG",
-  },
-];
-
-export default function PictureShowcase() {
+export default function PictureShowcase({ pictures }: { pictures: Picture[] }) {
   return (
     <div className="h-64">
       <Carousel pictures={pictures} />
